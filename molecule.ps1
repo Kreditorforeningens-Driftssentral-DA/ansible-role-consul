1324b57ee80101f93b3bcb747f52b85046cfa72b
Rune Rønneseth <rune.ronneseth@kred.no>
2019-12-19 22:43:14 +0100

    fix

--- molecule.ps1
+++ molecule.ps1
@@ -1,15 +1,17 @@
 param (
-    [String]$command = "list",
-    [String]$role    = "consul",
-    [String]$distro  = "centos:7",
-    [String]$cmd     = "/usr/sbin/init"
+    [string]$command  = "list",
+    [string]$scenario = "default",
+    [string]$role     = "consul",
+    [string]$distro   = "centos:7",
+    [string]$cmd      = "/usr/sbin/init"
 )
 
 docker run --rm -it `
   -v "$($pwd)/$($role):/tmp/$($role)" `
   -v "/var/run/docker.sock:/var/run/docker.sock" `
   -w "/tmp/$($role)" `
-  -e JOB_DOCKER_IMAGE="$($distro)" `
-  -e JOB_DOCKER_COMMAND="$($cmd)" `
+  -e TEST_IMAGE="$($distro)" `
+  -e TEST_IMAGE_CMD="$($cmd)" `
+  -e MOLECULE_NO_LOG=false `
   quay.io/ansible/molecule:latest `
-  molecule "$($command)"
+  molecule $command --scenario-name $scenario
